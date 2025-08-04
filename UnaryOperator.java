96eefbf30e1e8b133d1de5b43cf2ae411ecc39ea
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 16:29:27 +0530

    Add files via upload

---
+++ UnaryOperator.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class UnaryOperator {
+    /**
+     * java program to implement
+     * unary operators in java
+     * main function
+     */
+    public static void main(String[] args)
+        {
+            //integer declaration
+            int a=10;
+            int b=10;
+            //using unary operators
+            System.out.println("post-increment):"+ (a++));
+            System.out.println("pre-increment):"+ (++a));
+            System.out.println("post-decrement):"+ (b--));
+            System.out.println("post-decrement):"+ (--b));
+        }
+    }
+
