e20a66ad5e331f0a4dbdef6893a28e26e6894153
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 16:19:07 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class LogicalOperator {
+    // java program to implement
+//logical operator
+        //main function
+        public static void main(String[] ages){
+            // logical operators
+            boolean x=true;
+            boolean y=false;
+            System.out.println("x&&y:"+ false);
+            System.out.println("x||y:"+ true);
+            System.out.println("!x:"+(!x));
+        }
+    }
+
+
