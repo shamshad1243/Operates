de426101f052c466bb8f32dd6d34dd055898216a
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 16:33:49 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class ArithmeticOperator {
+
+        public static void main(String[] ages)
+        {
+            int a=10;
+            int b=20;
+            System.out.println("a+b="+(a+b));
+            System.out.println("a-b="+(a-b));
+            System.out.println("a*b="+(a*b));
+            System.out.println("a/b="+(0));
+            System.out.println("a%b="+(a%b));
+        }
+
+    }
