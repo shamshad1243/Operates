c8b326564e4fb547a2407cb0a0e2937bcb5631d9
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 11:32:04 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/src/operators/Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic {
+    public static void main(String agrm[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println("a+b="+(a+b));
+               System.out.println("a-b="+(a-b));
+                      System.out.println("a*b="+(a*b));
+                             System.out.println("a/b="+(a/b));
+                                    System.out.println("a%b="+(a%b));
+    }
+
+} 
+    
+
